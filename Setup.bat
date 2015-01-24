2161dab68eeedeca6cad2dc3d525fd53c7e83448
kylos101 <kylos101@gmail.com>
2015-01-24 17:54:55 -0500

    Updates post first successful test.

--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,5 @@
-Install-Chocolatey.bat
-Choco-Install-Software.bat
-gitAlias.bat
-NPM-Install-Software.bat
+cmd /c Install-Chocolatey.bat
+cmd /c Choco-Install-Software.bat
+cmd /c gitAlias.bat
+cmd /c NPM-Install-Software.bat
 regedit /s Command-Prompt-Settings.reg
