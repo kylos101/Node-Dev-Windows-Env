25722bb22d2c08fa3649d2a7a520a2cafd3c852a
kylos101 <kylos101@gmail.com>
2015-07-03 15:37:16 -0400

    Start grunt and open a cmd prompt for utility purposes

--- common scripts/open github command prompt.bat
+++ common scripts/open github command prompt.bat
@@ -1,1 +1,6 @@
-cmd /K "cd c:\Users\%username%\Documents\GitHub\"
+cd "c:\Users\%username%\Documents\GitHub\Plowapp"
+start /separate "cmd c:\Users\%username%\Documents\GitHub\Plowapp"
+cmd /C "grunt --force"
+
+
+
